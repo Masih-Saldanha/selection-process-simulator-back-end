acdbf7a7ba682b633c268765dedd135c07d6ba57
Masih <masih.saldanha@gmail.com>
2023-07-11 00:42:41 -0300

    chore: first commit

---
+++ src/test/java/com/ibm/teste_3/Teste3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.teste_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Teste3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
